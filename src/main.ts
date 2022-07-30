cd217b659945ddeb197e620edd04d93ded056cf5
santiagoBotina <santiagoboe04@gmail.com>
2022-07-29 21:00:46 -0500

    trying

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import express from 'express';
-import path from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(express.static(path.resolve(__dirname, '../../client/build')));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
